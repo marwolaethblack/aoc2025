18fd3883e526f65de677e0e9a8f06859fb0d05e5
Adnan Unal <a.unal677@gmail.com>
2025-12-03 22:45:33 +0100

    feat: day03 part 2

--- lib/day03/solution.ex
+++ lib/day03/solution.ex
@@ -54,7 +54,38 @@ defmodule AOC.Day03 do
   end
 
   def part2() do
+    input = parse_input("input.txt") |> parse_numbers
+
+    max_joltages =
+      Enum.reduce(input, 0, fn battery_array, acc ->
+        list_of_digits = number_to_digit_list(battery_array)
+
+        {_, digits} =
+          Enum.to_list(-11..0//+1)
+          |> Enum.reduce({list_of_digits, []}, fn x, {digit_list, digits} ->
+            list_without_end = Enum.drop(digit_list, x)
+
+            max = Enum.max(list_without_end)
+            max_index = Enum.find_index(digit_list, fn y -> y === max end)
+
+            new_digit_list = Enum.slice(digit_list, (max_index + 1)..length(digit_list))
+
+            {new_digit_list, digits ++ [max]}
+          end)
+
+        number =
+          digits
+          |> Enum.map(fn x -> Integer.to_string(x) end)
+          |> Enum.join()
+          |> String.to_integer()
+
+        acc + number
+      end)
+
+    IO.puts("Result: #{max_joltages}")
+    max_joltages
   end
 end
 
 AOC.Day03.part1()
+AOC.Day03.part2()
